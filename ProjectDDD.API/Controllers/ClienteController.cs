74419cafa7ff3c55e927643d5f109ff7d3e610af
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-09-04 08:35:50 -0300

    string de conexão e controller de cliente

---
+++ ProjectDDD.API/Controllers/ClienteController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectDDD.Application.DTOs;
+using ProjectDDD.Application.Interfaces;
+
+namespace ProjectDDD.API.Controllers
+{
+    [Route("v1/cliente")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        private readonly IClienteApplicationService _clienteApp;
+        public ClienteController(IClienteApplicationService clienteApp)
+        {
+            _clienteApp = clienteApp;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_clienteApp.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_clienteApp.GetAll());
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ClienteDTO clienteDTO)
+        {
+            try
+            {
+                if (clienteDTO == null)
+                    return NotFound();
+
+                _clienteApp.Add(clienteDTO);
+                return Ok("Cliente cadastrado com sucesso");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- ProjectDDD.API/Startup.cs
+++ ProjectDDD.API/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ProjectDDD.Infra.CrossCutting.IOC;
+using ProjectDDD.Infra.Data;
 
 namespace ProjectDDD.API
 {
@@ -25,7 +29,18 @@ namespace ProjectDDD.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["SqlConnection:SqlConnectionString"];
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API DDD", Version = "v1" });
+            });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ModuleIOC());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +55,11 @@ namespace ProjectDDD.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API DDD");
+            });
 
             app.UseHttpsRedirection();
 

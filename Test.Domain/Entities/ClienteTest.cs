71bfdfa03f00af6c7aece4ca4c4c87f86273c8c7
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-09-08 11:22:38 -0300

    remove projetos extras e add teste xUnit

---
+++ Test.Domain/Entities/ClienteTest.cs
@@ -0,0 +1,24 @@
+﻿using ProjectDDD.Domain.Entities;
+using System;
+using Xunit;
+
+namespace Test.Domain.Entities
+{
+    public class ClienteTeste
+    {
+        [Fact]
+        public void Cliente_deve_ser_valido()
+        {
+            var hoje = DateTime.Now;
+            Cliente cliente = new Cliente()
+            {
+                Nome = "Cliente 1",
+                Email = "cliente1@gmail.com",
+                Cadastro = hoje,
+                Codigo = 0
+            };
+
+            Assert.True(cliente != null && cliente.Nome == "Cliente 1" && cliente.Cadastro == hoje);
+        }
+    }
+}
